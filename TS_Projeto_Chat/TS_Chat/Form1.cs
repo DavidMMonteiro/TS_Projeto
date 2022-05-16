f36939d8a5b569218ea08b092f29081b168a2a66
David Machado Monteiro <92576744+DavidMMonteiro@users.noreply.github.com>
2022-05-16 11:59:25 +0100

    Chat form comment

--- TS_Projeto_Chat/TS_Chat/Form1.cs
+++ TS_Projeto_Chat/TS_Chat/Form1.cs
@@ -47,9 +47,13 @@ namespace TS_Projeto_Chat
         {
             try
             {
+                //Constroe uma mensagem para o servidor para fechar o canal
                 byte[] eot = protocolSI.Make(ProtocolSICmdType.EOT);
+                //Escreve a mensagem ao servidor
                 networkStream.Write(eot, 0, eot.Length);
+                // Espera pela resposta do servidor
                 networkStream.Read(protocolSI.Buffer, 0, protocolSI.Buffer.Length);
+                // Fecha o canal
                 networkStream.Close();
                 newMessage(this.name, "Fechar client... bye :-)");
                 client.Close();
@@ -61,25 +65,31 @@ namespace TS_Projeto_Chat
             }
         }
 
+        // Establece a ligação com o servidor
         private void connect_server()
         {
             try
             {
+                // Constreo a mensagem para ligar ao servidor
                 byte[] packet = protocolSI.Make(ProtocolSICmdType.DATA, this.name + "$");
+                // Envia a mensagem ao servidor
                 networkStream.Write(packet, 0, packet.Length);
                 newMessage(this.name , "Connected to server");
                 bt_send.Enabled = true;
             }
             catch (Exception ex)
             {
+                // Caso a ligação fallar, informa ao utilizador
                 newMessage(this.name , "Connection to server fail... Try later...");
                 consoleLog(ex.Message);
                 bt_send.Enabled = false;
             }
         }
 
+        // Envia a mensagem ao servidor
         private void send_message()
         {
+            // Valida que existe uma mensagem
             if (tb_message.Text == "")
                 return;
             string msg = tb_message.Text;
@@ -89,41 +99,51 @@ namespace TS_Projeto_Chat
                 newMessage(this.name, tb_message.Text);
                 tb_message.Clear();
                 byte[] packet = protocolSI.Make(ProtocolSICmdType.DATA, msg);
+                // Manda a mensagem ao servidor
                 networkStream.Write(packet, 0, packet.Length);
-                // Espera informa��o do servidor
+                // Espera informação do servidor
                 while (protocolSI.GetCmdType() != ProtocolSICmdType.ACK)
                     networkStream.Read(protocolSI.Buffer, 0, protocolSI.Buffer.Length);
 
             }
             catch (Exception ex)
             {
+                // Caso a mensagem sofrer erro ao ser enviada
                 newMessage(this.name, "Erro ao comunicar com o servidor.\r\n" + ex.Message);
                 bt_send.Enabled = false;
             }
         }
+
+        // Establece ligação com o servidor
         private void bt_connect_Click(object sender, EventArgs e)
         {
             connect_server();
         }
 
+        // Quando o butão e clickado envia a mensagem ao servidor
         private void bt_send_Click(object sender, EventArgs e)
         {
             send_message();
         }
 
+        // Quando uma tecla e clickada envia a mensagem ao servidor
         private void tb_message_KeyPress(object sender, KeyPressEventArgs e)
         {
-            if (e.KeyChar == (char)Keys.Enter)
-            {
+            // Valida a tecla a ser clickada, neste caso enter
+            if (e.KeyChar == (char)Keys.Enter)    
                 send_message();
-            }
         }
+
+        // Quando o form é fechado, fecha o canal do cliente com o servidor
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
             CloseClient();
+            // Fecha a aplicação por completo
             Application.Exit();
         }
 
+        // Efetua o logout do cliente, fechado a ligação já establecido 
+        // e abre o form de login para o utilizador 
         private void bt_logout_Click(object sender, EventArgs e)
         {
             CloseClient();

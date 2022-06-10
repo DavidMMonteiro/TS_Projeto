1dc6eacd973bffabd60516cb5e4f3b0c29842913
David Machado Monteiro <2211849@my.ipleiria.pt>
2022-06-10 17:40:48 +0100

    Minor Corrections

--- TS_Projeto_Chat/Server/Cryptor.cs
+++ TS_Projeto_Chat/Server/Cryptor.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.IO;
 using System.Security.Cryptography;
+using System.Text;
 
 namespace TS_Chat
 {
@@ -7,6 +9,12 @@ namespace TS_Chat
     {
         private int SALT_SIZE = 8;
         private int ITERATIONS = 1000;
+        private AesCryptoServiceProvider AES;
+
+        public Cryptor()
+        {
+            this.AES = new AesCryptoServiceProvider();
+        }
 
         public byte[] GenerateSalt()
         {
@@ -20,6 +28,66 @@ namespace TS_Chat
         {
             Rfc2898DeriveBytes rfc2898 = new Rfc2898DeriveBytes(plainText, salt, this.ITERATIONS);
             return rfc2898.GetBytes(32);
+        }
+
+        //Create Simetric Key from string
+        public string CreatePrivateKey(string pass)
+        {
+            byte[] salt = new byte[] { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 };
+            Rfc2898DeriveBytes pwgen = new Rfc2898DeriveBytes(pass, salt, this.ITERATIONS);
+            //Generate key
+            byte[] key = pwgen.GetBytes(16);
+            //Converte key to Base64 and return
+            return Convert.ToBase64String(key);
+        }
+
+        //Create Initial Vector from string
+        public string CreateIV(string pass)
+        {
+            byte[] salt = new byte[] { 3, 14, 15, 92, 65, 35, 89, 79, 32, 38, 46 };
+            Rfc2898DeriveBytes pwgen = new Rfc2898DeriveBytes(pass, salt, ITERATIONS);
+            //Generate initicial vetor
+            byte[] iv = pwgen.GetBytes(16);
+            //Converte initicial vetor to Base64 and return
+            return Convert.ToBase64String(iv);
+        }
+
+        //Encrypt a string with AES 
+        public string EncryptText(string text_to_encrypt)
+        {
+            //Save desencrypted text as Bytes
+            byte[] desencrypted_text = Encoding.UTF8.GetBytes(text_to_encrypt);
+            //Save encryoted text as Bytes
+            byte[] encrypted_text;
+            //Save memory space
+            MemoryStream ms = new MemoryStream();
+            //Initialize encrypted sistem
+            CryptoStream cs = new CryptoStream(ms, AES.CreateEncryptor(), CryptoStreamMode.Write);
+            //Encrypt data
+            cs.Write(desencrypted_text, 0, desencrypted_text.Length);
+            cs.Close();
+            //Save encrypted data from memory
+            encrypted_text = ms.ToArray();
+            //Convert from Byte -> Base64 and return
+            return Convert.ToBase64String(encrypted_text);
+        }
+
+        //Desencrypt string
+        public string DesencryptText(string text_encrypted)
+        {
+            //Save desencrypted text as Bytes
+            byte[] encrypted_text = Convert.FromBase64String(text_encrypted);
+            //Save memory space
+            MemoryStream ms = new MemoryStream(encrypted_text);
+            //Initialize encrypted sistem
+            CryptoStream cs = new CryptoStream(ms, AES.CreateDecryptor(), CryptoStreamMode.Read);
+            //Save desencrypt text
+            byte[] desencrypted_text = new byte[ms.Length];
+            //Number of bytes desencrypted
+            int readBytes = cs.Read(desencrypted_text, 0, desencrypted_text.Length);
+            cs.Close();
+            //Convert from Byte -> Base64 and return
+            return Encoding.UTF8.GetString(desencrypted_text, 0, readBytes);
         }
 
     }
--- TS_Projeto_Chat/TS_Chat/Form_Login.cs
+++ TS_Projeto_Chat/TS_Chat/Form_Login.cs
@@ -10,10 +10,12 @@ namespace TS_Projeto_Chat
     public partial class Form_Login : Form
     {
         // Atributos para conneção com  o Servidor 
-        private const int port = 10000;
-        private NetworkStream networkStream;
-        private ProtocolSI protocolSI;
-        private TcpClient client;
+        private const int PORT = 10000;
+        private NetworkStream NetworkStream;
+        private ProtocolSI ProtocolSI;
+        private TcpClient Client;
+        private string PrivateKey;
+        private string Vetor;
 
         public Form_Login()
         {
@@ -35,15 +37,15 @@ namespace TS_Projeto_Chat
             try
             {
                 // Cria a localização do servidor (IP local e porta)
-                IPEndPoint endPoint = new IPEndPoint(IPAddress.Loopback, port);
+                IPEndPoint endPoint = new IPEndPoint(IPAddress.Loopback, PORT);
                 // Inicializa o TCPclientes
-                client = new TcpClient();
+                Client = new TcpClient();
                 // Vas a ligação com o servição
-                client.Connect(endPoint);
+                Client.Connect(endPoint);
                 // Abre a ligação com o servidor
-                networkStream = client.GetStream();
+                NetworkStream = Client.GetStream();
                 // Inicializa a class ProtocolISI
-                protocolSI = new ProtocolSI();
+                ProtocolSI = new ProtocolSI();
                 return true;
             }
             catch (Exception ex)
@@ -69,19 +71,22 @@ namespace TS_Projeto_Chat
                 //Constroe a mensagem do cliente
                 //TODO  Encrypte password & message to server
                 string msg = username + "$" + password;
-                protocolSI = new ProtocolSI();
+                ProtocolSI = new ProtocolSI();
                 // Converte a mensagem para bytes para poder ser enviada
-                byte[] packet = protocolSI.Make(ProtocolSICmdType.ACK, msg);
+                byte[] packet = ProtocolSI.Make(ProtocolSICmdType.ACK, msg);
                 // Faz a transmição da mensagem
-                networkStream.Write(packet, 0, packet.Length);
+                NetworkStream.Write(packet, 0, packet.Length);
                 // Espera pela informação do servidor
                 do { 
-                    networkStream.Read(protocolSI.Buffer, 0, protocolSI.Buffer.Length);
-                } while (protocolSI.GetCmdType() != ProtocolSICmdType.ACK);
+                    NetworkStream.Read(ProtocolSI.Buffer, 0, ProtocolSI.Buffer.Length);
+                } while (ProtocolSI.GetCmdType() != ProtocolSICmdType.ACK);
                 // Lee a informação da mensagem returnada pelo servidor
-                string serermsg = protocolSI.GetStringFromData();
+                string serermsg = ProtocolSI.GetStringFromData();
+                bool loginState = bool.Parse(serermsg.Split('$')[0]);
+                this.PrivateKey = serermsg.Split('$')[1];
+                this.Vetor = serermsg.Split('$')[2];
                 // Converte para bool
-                return bool.Parse(serermsg);
+                return loginState;
             }
             catch (Exception ex)
             {
@@ -115,14 +120,14 @@ namespace TS_Projeto_Chat
             }
 
             // Se for bem sucedido, abré os chats ao cliente
-            Form1 chat = new Form1(port, networkStream, protocolSI, client, tb_user.Text);
+            Form1 chat = new Form1(PORT, NetworkStream, ProtocolSI, Client, tb_user.Text);
             chat.Show();
             this.Hide();
         }
 
         private void bt_singup_Click(object sender, EventArgs e)
         {
-            new FormSignUp(this, port, networkStream, protocolSI, client).ShowDialog();
+            new FormSignUp(this, PORT, NetworkStream, ProtocolSI, Client).ShowDialog();
         }
 
         private void Form_Login_FormClosing(object sender, FormClosingEventArgs e)
